34d7352b35f21013edc6c9d1e577315a574af71b
Albin <albin.svensson.7131@student.uu.se>
2026-03-12 13:36:16 +0000

    updated preprocessing, now we remove 264 and 265

--- download_data.sh
+++ download_data.sh
@@ -1,10 +1,10 @@
 mkdir -p taxi_data
 
-for year in $(seq 2012 2022)
+for year in $(seq 2012 2018)
 do
   for month in $(seq -w 1 12)
   do
     url="https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_${year}-${month}.parquet"
-    wget -nc -P taxi_data --user-agent="Mozilla/5.0" --referer="https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page" "$url" || true
+    wget -nc -P ~/taxi_data --user-agent="Mozilla/5.0" --tries=10 --waitretry=2 --timeout=30 --wait=2 --random-wait --limit-rate=1m --referer="https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page" "$url" || true
   done
 done
--- src/etl_job.py
+++ src/etl_job.py
@@ -5,6 +5,9 @@ def load_and_filter(spark, input_path):
 
     #Removes all trips with a distance of 0
     df = df.filter(col("trip_distance") > 0)
+    #Remove all unknown zones
+    df = df.filter((col("PULocationID") != 264) & (col("PULocationID") != 265))
+    df = df.filter((col("DOLocationID") != 264) & (col("DOLocationID") != 265))
     #Get the hour from the datetime
     df = df.withColumn("time_stamp",hour(col("tpep_pickup_datetime")))
     #Create column with route
