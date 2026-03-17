9d78e7fa327623f6845ef54ea6a7d67da315c96d
danielalariksson01 <daniel.alariksson@gmail.com>
2026-03-17 13:50:08 +0100

    Added weak scaling code to config

--- src/config.py
+++ src/config.py
@@ -2,6 +2,21 @@
 HDFS_INPUT_PATH  = "hdfs:///data/taxi/*.parquet" # Dataset location
 HDFS_OUTPUT_PATH = "hdfs:///output/avg_trip_time" # Output location
 
+#FOR WEAK SCALING
+""""
+#2012-2013 dataset ~ 28 GB
+HDFS_INPUT_PATH  = "hdfs:///data/taxi/yellow_tripdata_201[23]-*.parquet"
+HDFS_OUTPUT_PATH = "hdfs:///output/run1_weak_scaling"
+
+#2012-2015 dataset ~ 48 GB
+HDFS_INPUT_PATH  = "hdfs:///data/taxi/yellow_tripdata_201[2345]-*.parquet"
+HDFS_OUTPUT_PATH = "hdfs:///output/run2_weak_scaling
+
+#2012-2017 dataset ~ 72 GB
+HDFS_INPUT_PATH  = "hdfs:///data/taxi/yellow_tripdata_201[234567]-*.parquet"
+HDFS_OUTPUT_PATH = "hdfs:///output/run3_weak_scaling
+"""
+
 '''Spark'''
 APP_NAME         = "NYC Taxi Zone Analysis" # Name of the application
 #MASTER_URL = "local[*]" #Only for master node testing
